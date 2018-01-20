0d03676f0b64d98bb7a694f45293b030ea78cb16
Heeryong Kang <drakang4@users.noreply.github.com>
2018-01-20 21:51:07 +0900

    Fixed channel element id

--- src/gatsby-browser.js
+++ src/gatsby-browser.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 exports.onRouteUpdate = ({ location }, { pages }) => {
   if (window.CHPlugin && Array.isArray(pages)) {
-    const button = document.getElementById('ch-plugin-button-position');
+    const button = document.getElementById('ch-plugin');
     if (pages.indexOf(location.pathname) === -1) {
       button.style.display = 'none';
     } else {
